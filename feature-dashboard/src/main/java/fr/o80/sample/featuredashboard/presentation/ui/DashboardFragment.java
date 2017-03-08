f2eb278e7df5bcbf20a7819750b2b6405c9dab5d
Olivier PEREZ <olivier@olivierperez.fr>
2017-03-08 20:26:15 +0100

    Create base for Fragments

--- feature-dashboard/src/main/java/fr/o80/sample/featuredashboard/presentation/ui/DashboardFragment.java
+++ feature-dashboard/src/main/java/fr/o80/sample/featuredashboard/presentation/ui/DashboardFragment.java
@@ -1,26 +1,23 @@
 package fr.o80.sample.featuredashboard.presentation.ui;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import javax.inject.Inject;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
 import fr.o80.sample.featuredashboard.DashboardService;
 import fr.o80.sample.featuredashboard.R;
 import fr.o80.sample.featuredashboard.R2;
+import fr.o80.sample.lib.core.ui.BaseFragment;
 
 /**
  * @author Olivier Perez
  */
-public class DashboardFragment extends Fragment {
+public class DashboardFragment extends BaseFragment {
 
     @Inject
     protected DashboardService dashboardService;
@@ -35,18 +32,15 @@ public class DashboardFragment extends Fragment {
         ((DashboardActivity)getActivity()).component().inject(this);
     }
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        ButterKnife.bind(this, view);
-        return view;
-    }
-
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         textView.setText(R.string.lorem_ipsum);
         Toast.makeText(getActivity(), dashboardService.getTitle(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_dashboard;
     }
 }
---
+++ lib/src/main/java/fr/o80/sample/lib/core/ui/BaseFragment.java
@@ -0,0 +1,39 @@
+package fr.o80.sample.lib.core.ui;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+/**
+ * Base Fragment for every
+ * @author Olivier Perez
+ */
+public abstract class BaseFragment extends Fragment {
+
+    private Unbinder unbinder;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(getLayoutId(), container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        unbinder.unbind();
+        super.onDestroyView();
+    }
+
+    @LayoutRes
+    protected abstract int getLayoutId();
+
+}
