b86a8723ebe27d5329269581b2d14087cd0d4d1e
Olivier PEREZ <olivier@olivierperez.fr>
2017-03-16 00:29:12 +0100

    Start scanning QR codes

--- feature-qr-scan/src/main/java/fr/o80/featureqrscan/presentation/ui/QRScanActivity.java
+++ feature-qr-scan/src/main/java/fr/o80/featureqrscan/presentation/ui/QRScanActivity.java
@@ -1,32 +1,41 @@
 package fr.o80.featureqrscan.presentation.ui;
 
-import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-import fr.o80.featureqrscan.DaggerQRScanComponent;
-import fr.o80.featureqrscan.QRScanComponent;
-import fr.o80.sample.lib.core.LibApplication;
-import fr.o80.sample.lib.core.ui.BaseDrawerActivity;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.CaptureActivity;
 
 /**
+ * This class starts the QR code scanning and handle the result.
+ *
  * @author Olivier Perez
  */
-public class QRScanActivity extends BaseDrawerActivity {
-
-    private QRScanComponent component;
+public class QRScanActivity extends CaptureActivity {
 
     @Override
-    protected void initDagger() {
-        component = DaggerQRScanComponent.builder()
-                .libComponent(((LibApplication) getApplication()).component())
-                .build();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new IntentIntegrator(this).initiateScan();
     }
 
     @Override
-    protected Fragment getInitFragment() {
-        return QRScanFragment.newInstance();
-    }
-
-    public QRScanComponent component() {
-        return component;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Log.d("QRScanActivity", "Cancelled scan");
+                finish();
+            } else {
+                Log.d("QRScanActivity", "Scanned");
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_SHORT).show();
+                // TODO Do somthing cool
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
