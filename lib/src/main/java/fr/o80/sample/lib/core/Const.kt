501d3556b38e9c4d2c6c4e71a7bb307c7a0f800c
Olivier Perez [a570709] <olivier.perez@worldline.com>
2018-04-25 11:49:09 +0000

    Add missing const file

---
+++ lib/src/main/java/fr/o80/sample/lib/core/Const.kt
@@ -0,0 +1,11 @@
+package fr.o80.sample.lib.core
+
+/**
+ * @author Olivier Perez
+ */
+object Const {
+    const val KEY_ENTRIES = "KEY_ENTRIES"
+    const val KEY_TOTALHOURS = "KEY_TOTALHOURS"
+    const val KEY_DATE = "KEY_DATE"
+    const val HOURS_PER_DAY = 8
+}
