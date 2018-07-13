b0154580bda2b9cfd09e1d817aef5e08cf360238
Olivier Perez [a570709] <olivier.perez@worldline.com>
2018-07-13 08:30:56 +0000

    Fix the build of Dagger in the BOOT_COMPLETE receiver

--- feature-reminder/src/main/java/fr/o80/featurereminder/receiver/OnStartupReceiver.kt
+++ feature-reminder/src/main/java/fr/o80/featurereminder/receiver/OnStartupReceiver.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import fr.o80.featurereminder.ScheduleRemind
 import fr.o80.featurereminder.dagger.DaggerReminderComponent
+import fr.o80.sample.lib.core.LibApplication
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -16,6 +17,7 @@ class OnStartupReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent?) {
         Timber.i("Schedule the reminder at phone boot")
         DaggerReminderComponent.builder()
+                .libComponent((context as LibApplication).component)
                 .build()
                 .inject(this)
         scheduler.scheduleReminder()
